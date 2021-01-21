90a7aa98b6a202cd8040525dcfa197e88543ad21
杨航 <hang.yang@jibaoyuan.net>
2021-01-22 00:05:30 +0800

    修改配置

--- nuxt.config.js
+++ nuxt.config.js
@@ -93,6 +93,19 @@ export default {
 
     // Build Configuration (https://go.nuxtjs.dev/config-build)
     build: {
-        transpile: [/^element-ui/]
+        transpile: [/^element-ui/],
+        extractCSS: true,
+        optimization: {
+            splitChunks: {
+                cacheGroups: {
+                    styles: {
+                        name: 'styles',
+                        test: /\.(css|vue)$/,
+                        chunks: 'all',
+                        enforce: true
+                    }
+                }
+            }
+        }
     }
 }
